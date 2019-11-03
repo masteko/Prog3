47e0b0b557ad28d2bd87d5685bd22b33378ba152
Manuel <njester5030@gmail.com>
2019-11-03 16:37:30 +0100

    refactoring done

--- PerfekteZahlen/main.cpp
+++ PerfekteZahlen/main.cpp
@@ -4,33 +4,34 @@
 using namespace std;
 
 int main() {
-	bool timeCheck{ true };
+	bool searching{ true };
 	int number{ 5 };
-	time_t timeBefore = time(nullptr);
+	time_t startTime = time(nullptr);
 
-	while (timeCheck) {
-		int sum{ 0 };
-		int dividorCount{ 0 };
-		int Dividors[1000];
+	while (searching) {
+		auto sum{ 0 };
+		auto dividersCount{ 0 };
+		int dividers[100]{ 0 };
 
 		++number;
 		for (auto i{ number - 1 }; i > 0; --i) {
 			if (number % i == 0) {
-				Dividors[dividorCount] = i;
+				dividers[dividersCount] = i;
 				sum += i;
-				++dividorCount;
+				++dividersCount;
 			}
 		}
-		timeCheck = (time(nullptr) - timeBefore) < 120;
 
 		if (sum == number) {
 			cout << "Perfekte Zahl gefunden: " << number << " (";
-			for (auto i{ dividorCount - 1}; i >= 0; --i) {
-				cout << Dividors[i] << (i > 0 ? " + " : "");
+			for (auto i{ dividersCount - 1}; i >= 0; --i) {
+				cout << dividers[i] << (i > 0 ? " + " : "");
 			}
 			cout << ")" << endl;
-			timeBefore = time(nullptr);
+			startTime = time(nullptr);
 		}
+
+		searching = (time(nullptr) - startTime) < 120;
 	};
 
 	std::cout << "Letzte Zahl: " << number << endl;
