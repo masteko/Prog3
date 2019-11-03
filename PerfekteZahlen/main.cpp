e1dbe709be9d2323e1317f2031c80df2250a151a
Manuel <njester5030@gmail.com>
2019-11-03 16:24:16 +0100

    perfect number implementation finished

--- PerfekteZahlen/main.cpp
+++ PerfekteZahlen/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 #include <ctime>
 
-int main() {
+using namespace std;
 
+int main() {
+	bool timeCheck{ true };
+	int number{ 5 };
+	time_t timeBefore = time(nullptr);
+
+	while (timeCheck) {
+		int sum{ 0 };
+		int dividorCount{ 0 };
+		int Dividors[1000];
+
+		++number;
+		for (auto i{ number - 1 }; i > 0; --i) {
+			if (number % i == 0) {
+				Dividors[dividorCount] = i;
+				sum += i;
+				++dividorCount;
+			}
+		}
+		timeCheck = (time(nullptr) - timeBefore) < 120;
+
+		if (sum == number) {
+			cout << "Perfekte Zahl gefunden: " << number << " (";
+			for (auto i{ dividorCount - 1}; i >= 0; --i) {
+				cout << Dividors[i] << (i > 0 ? " + " : "");
+			}
+			cout << ")" << endl;
+			timeBefore = time(nullptr);
+		}
+	};
+
+	std::cout << "Letzte Zahl: " << number << endl;
+	return 0;
 }
