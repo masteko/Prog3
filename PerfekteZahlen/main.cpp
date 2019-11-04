ff266177fa2b17162033e092206141594194440c
Manuel <njester5030@gmail.com>
2019-11-04 12:12:34 +0100

    refactoring add magic numbers

--- PerfekteZahlen/main.cpp
+++ PerfekteZahlen/main.cpp
@@ -7,15 +7,17 @@ int main() {
 	auto searching{ true };
 	auto number{ 5 };
 	auto startTime = time(nullptr);
+	const int MaxCalcTime{ 120 };
 
 	while (searching) {
 		auto sum{ 0 };
+		const int MaxDividers{ 1000 };
 		auto dividersCount{ 0 };
-		int dividers[100]{ 0 };
+		int dividers[MaxDividers];
 
 		++number;
 		for (auto i{ number - 1 }; i > 0; --i) {
-			if (number % i == 0) {
+			if (0 == number % i) {
 				dividers[dividersCount] = i;
 				sum += i;
 				++dividersCount;
@@ -31,7 +33,7 @@ int main() {
 			startTime = time(nullptr);
 		}
 
-		searching = (time(nullptr) - startTime) < 120;
+		searching = (time(nullptr) - startTime) < MaxCalcTime;
 	};
 
 	std::cout << "Letzte Zahl: " << number << endl;
