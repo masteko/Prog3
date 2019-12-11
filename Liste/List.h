0c3516d31ef1dc98231eca605eb1efb376ee01d1
Manuel <njester5030@gmail.com>
2019-12-11 20:16:33 +0100

    baby steps :)

--- Liste/List.h
+++ Liste/List.h
@@ -1,101 +1,155 @@
 #pragma once
 #include <stdexcept>
+#include <iostream>
 
-struct Node {
-	float val;
-	Node* next;
-};
+namespace cns {
 
-class List
-{
-public:
-	List() : head(nullptr) {};
-	List(const List& src) : List() {
-		copyList(src.head);
-	};
-	virtual ~List() {
-		if (nullptr != head) {
-			deleteEntries(head);
-		}
-		head = nullptr;
-	};
-	Node* getHead() {
-		return head;
-	};
-	bool isEmpty() const {
-		return nullptr == head;
-	};
-	void pushback(float val) {
-		if (nullptr == head) {
-			createHead(val);
-			return;
-		}
-		Node* tail = head;
-		if (nullptr != tail) {
-			while (nullptr != tail->next) {
-				tail = tail->next;
-			}
-		}
-		tail->next = new Node{ val, nullptr };
-	};
-	float& operator[](size_t index) {
-		if (index >= getSize()) {
-			throw std::logic_error("Index out of bounds");
-		}
-		else {
-			return getValAt(index);
-		}
-	};
-	List& operator=(const List& src) {
-		if (this != &src) {
-			deleteEntries(head);
+	template<typename T>
+	class List {
+	public:
+		List() : head(nullptr) {}
+		List(const List& src) : List() {
 			copyList(src.head);
 		}
-		return *this;
-	};
-private:
-	void createHead(float val) {
-		head = new Node{ val, nullptr };
-	};
-	void copyList(Node* src) {
-		Node* srcNode = src;
-		if (nullptr != srcNode) {
-			head = new Node{ srcNode->val, nullptr };
+		List(List&& src) : List() {
+			*this = std::move(src);
+		}
+		~List() {
+			emptyList();
+		}
+		List& operator=(const List& src) {
+			if (this != &src) {
+				emptyList();
+				copyList(src.head);
+			}
+			return *this;
+		}
+		List& operator=(List&& src) {
+			if (this != &src) {
+				emptyList();
+				head = src.head;
+				src.head = nullptr;
+			}
+			return *this;
+		};
+		T& operator[](size_t index) const {
+			return getNodeAtIndex(index)->value;
+		}
+		void removeAt(size_t index) {
+			Node* oldNode;
 
-			while (nullptr != srcNode->next) {
-				srcNode = srcNode->next;
-				pushback(srcNode->val);
+			if (0 == index) {
+				oldNode = getNodeAtIndex(0);
+				head = oldNode->next;
+			}
+			else {
+				Node* prev = getNodeAtIndex(index - 1);
+				oldNode = prev->next;
+
+				if (nullptr == oldNode) {
+					throw std::logic_error("Index out of bounds");
+				}
+
+				prev->next = oldNode->next;
+			}
+
+			delete oldNode;
+		}
+		void pushback(T val) {
+			if (nullptr == head) {
+				head = new Node{ val, nullptr };
+				return;
+			}
+
+			Node* last = getLast();
+			last->next = new Node{ val, nullptr };
+		}
+		size_t size() const {
+			size_t len{ 0 };
+
+			if (isEmpty()) return len;
+
+			Node* current = head;
+
+			while (nullptr != current) {
+				current = current->next;
+				++len;
+			}
+
+			return len;
+		}
+		bool isEmpty() const {
+			return nullptr == head;
+		}
+		const void* const getPtrAt(size_t index) const {
+			return getNodeAtIndex(index);
+		}
+	private:
+		struct Node {
+			T value;
+			Node* next;
+		};
+		Node* head;
+		Node* getNodeAtIndex(size_t index) const {
+			Node* current = head;
+
+			while (nullptr != current && index > 0) {
+				current = current->next;
+				--index;
+			}
+
+			if (0 == index && !isEmpty()) {
+				return current;
+			}
+
+			throw std::logic_error("Index out of bounds");
+		}
+		void copyList(Node* src) {
+			if (nullptr == src) {
+				return;
+			}
+
+			head = new Node{ src->value, nullptr };
+			src = src->next;
+			Node* current = head;
+
+			while (nullptr != src) {
+				current = new Node{ src->value, nullptr };
+				src = src->next;
 			}
 		}
-	};
-	size_t getSize() {
-		size_t size{ 0 };
-		Node* node = head;
-		if (nullptr != head) {
-			size++;
-			while (nullptr != node->next) {
-				size++;
-				node = node->next;
-			}
-		}
-		return size;
-	};
-	float& getValAt(size_t index) {
-		size_t idx{ 0 };
-		Node* node = head;
-		float val;
-		while (index != idx) {
-			node = node->next;
-			idx++;
-		}
-		return node->val;
-	};
-	void deleteEntries(Node* node) {
-		if (nullptr != node->next) {
-			deleteEntries(node->next);
-		}
-		delete node;
-	};
-	Node* head;
-};
+		void emptyList() {
+			Node* current = head;
 
+			while (nullptr != current) {
+				Node* next = current->next;
+				delete current;
+				current = next;
+			}
+
+			head = nullptr;
+		}
+		Node* getLast() {
+			Node* current = head;
+
+			while (nullptr != current->next) {
+				current = current->next;
+			}
+
+			return current;
+		}
+	};
+
+	template<typename T>
+	std::ostream& operator<<(std::ostream& out, const List<T>& data) {
+		auto size = data.size();
+	
+		out << "Liste mit " << size << " items: \n\n";
+
+		for (size_t i = 0; i < size; ++i) {
+			out << data[i] << (i < size -1 ? "\n-----------------\n" : "");
+		}
+
+		return out;
+	}
+}
--- Liste/main.cpp
+++ Liste/main.cpp
@@ -3,6 +3,18 @@
 #include <iostream>
 #include <string>
 
+struct Test {
+	int val;
+	double dval;
+};
+
+std::ostream& operator<<(std::ostream& out, const Test& data) {
+	out << "Test Objekt mit den Werten:\nval: " << data.val << "\ndval: " << data.dval;
+	return out;
+}
+
+using List = cns::List<Test>;
+
 void checkMemory() {
 	assert(_CrtCheckMemory());
 	assert(false == _CrtDumpMemoryLeaks());
@@ -15,18 +27,18 @@ void List_DefaultConstructorIsEmpty() {
 
 void List_pushback_InsertElementAtListEndAndListIsNotMoreEmpty() {
 	List list;
-	list.pushback(1.2f);
+	list.pushback(Test{ 1, 1.2 });
 	assert(false == list.isEmpty());
 }
 
 void List_pushback_InsertThreeElementsAtListAndReadThemWithIndexOperator() {
 	List list;
-	list.pushback(1.2f);
-	list.pushback(2.2f);
-	list.pushback(3.2f);
-	assert(1.2f == list[0]);
-	assert(2.2f == list[1]);
-	assert(3.2f == list[2]);
+	list.pushback(Test{ 1, 1.2 });
+	list.pushback(Test{ 2, 2.2 });
+	list.pushback(Test{ 3, 3.3 });
+	assert(1 == list[0].val);
+	assert(2 == list[1].val);
+	assert(3 == list[2].val);
 }
 
 void List_index_outOfBounds() {
@@ -36,26 +48,90 @@ void List_index_outOfBounds() {
 		list[0];
 		std::cerr << "Index out of bound expected";
 	} catch (const std::logic_error & ex) {
-		assert(std::strcmp("Index out of bounds", ex.what()) == 0);
+		assert(0 == std::strcmp("Index out of bounds", ex.what()));
 	}
 }
 
 void List_copyConstructor() {
 	List list;
-	list.pushback(1.2);
+	list.pushback(Test{ 1, 1.2 });
 
 	List list2{ list };
-	assert(list[0] == list2[0]);
+	assert(list[0].val == list2[0].val);
 	assert(&list[0] != &list2[0]);
 }
 
 void List_delegateOperator() {
 	List list;
-	list.pushback(1.2);
+	list.pushback(Test{ 1, 1.2 });
 
 	List list2 = list;
-	assert(list[0] == list2[0]);
+	assert(list[0].val == list2[0].val);
 	assert(&list[0] != &list2[0]);
+}
+
+void List_remove_Elements() {
+	List list;
+	list.pushback(Test{ 1, 1.2 });
+	list.pushback(Test{ 2, 2.2 });
+	list.pushback(Test{ 3, 3.2 });
+	list.pushback(Test{ 4, 4.4 });
+
+	list.removeAt(2);
+	assert(4 == list[2].val);
+	assert(2 == list[1].val);
+
+	list.removeAt(0);
+	assert(2 == list[0].val);
+
+	list.removeAt(1);
+	assert(1 == list.size());
+
+	list.removeAt(0);
+	assert(list.isEmpty());
+}
+
+void List_remove_ElementsAtWrongIndex() {
+	List list;
+
+	try {
+		list.removeAt(0);
+		std::cerr << "Index out of bounds expected";
+	}
+	catch (const std::logic_error ex) {
+		assert(0 == std::strcmp("Index out of bounds", ex.what()));
+	}
+
+	list.pushback(Test{ 1, 1.2 });
+
+	try {
+		list.removeAt(1);
+		std::cerr << "Index out of bounds expected";
+	}
+	catch (const std::logic_error ex) {
+		assert(0 == std::strcmp("Index out of bounds", ex.what()));
+	}
+}
+
+void List_moveConstructor() {
+	List list;
+	list.pushback(Test{ 1, 1.2 });
+	list.pushback(Test{ 2, 2.2 });
+	auto n = list.getPtrAt(1);
+
+	List list2{ std::move(list) };
+	assert(list.isEmpty());
+	assert(n == list2.getPtrAt(1));
+	assert(1 == list2[0].val);
+	assert(2 == list2[1].val);
+}
+
+void List_streamOperator() {
+	List list;
+	list.pushback(Test{ 1, 1.2 });
+	list.pushback(Test{ 2, 2.2 });
+
+	std::cout << list << std::endl;
 }
 
 int main() {
@@ -67,6 +143,10 @@ int main() {
 		List_index_outOfBounds,
 		List_copyConstructor,
 		List_delegateOperator,
+		List_remove_Elements,
+		List_remove_ElementsAtWrongIndex,
+		List_moveConstructor,
+		List_streamOperator,
 	};
 
 	for (size_t i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {
